3a0ed307599daf367ffd5ae368fb29a7a471e4dd
Avik Sengupta <avik@sengupta.net>
2024-07-01 13:39:17 +0100

    make tests run

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,22 +3,21 @@ using Test
 using Redis
 
 global x = 1
-struct T1 <:HorusJob
+struct TestJob1 <:HorusJob
     a
 end
 
-function Horus.execute(t::T1)
-    @show t.a
+function Horus.execute(t::TestJob1)
     global x = x + t.a
-    error("there was an error")
 end
 
 #This test assumes redis running on localhost
 @testset "Horus.jl" begin
-    conn = RedisConnection(;host="172.23.164.254", port=6379)
+    #conn = RedisConnection(;host="172.23.164.254", port=6379)
+    conn = RedisConnection(;host="localhost", port=6379)
     conf = Horus.HorusClientConfig(conn)
 
-    Horus.enqueue(conf, T1, 1)
+    Horus.enqueue(conf, TestJob1, 1)
 
     sconf = Horus.HorusServerConfig(conn)
     Horus.run_job(sconf, Horus.fetch_job(sconf))
