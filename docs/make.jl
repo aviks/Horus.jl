418d7327df341f670299a119a1ea094dbeddaccf
Avik Sengupta <avik@sengupta.net>
2024-07-02 20:01:46 +0100

    try to fix doc building on juliahub

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,6 @@ DocMeta.setdocmeta!(Horus, :DocTestSetup, :(using Horus); recursive=true)
 makedocs(;
     modules=[Horus],
     authors="Avik Sengupta <avik@sengupta.net> and contributors",
-    repo="https://github.com/aviks/Horus.jl/blob/{commit}{path}#{line}",
     sitename="Horus.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
